69abf14f98d50b5e8ecb86559991956f51ffcb1b
Talha Siddique <Talhasiddique375@gmail.com>
2023-08-11 23:55:28 +0200

    Search Bar and Option Element Added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -22,10 +24,45 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  // dont add the state variables here since they will render each time when state is changed. So use it in submit handler
+  function HandlerSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const itemsCheckList = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    setDescription("");
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={HandlerSubmit}>
       <h3>What do you need for trip 😍</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Items..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      ></input>
+
+      <button> ADD</button>
+    </form>
   );
 }
 function PackingList() {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  // This StrictMode produces double renders for further checking??
+  <App />
+  // </React.StrictMode>
 );
