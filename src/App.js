b77ba62e6a0d3c7e4cdac18508a2b9dc1d3f7ca6
Talha Siddique <Talhasiddique375@gmail.com>
2023-08-11 22:07:01 +0200

    List features implemented, select and search items will be later added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Passports", quantity: 2, packed: false },
+  { id: 4, description: "Socks", quantity: 12, packed: false },
+  { id: 5, description: "Passports", quantity: 2, packed: false },
+  { id: 6, description: "Socks", quantity: 12, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for trip 😍</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((items) => (
+          <Item items={items} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ items }) {
+  return (
+    <li key={items.id}>
+      <span style={items.packed ? { textDecoration: "line-through" } : {}}>
+        {items.quantity} {items.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
